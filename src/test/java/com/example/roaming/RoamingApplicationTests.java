68f9154ba1b4477af9570d326c08181459d3db71
rka0608 <92374176+rka0608@users.noreply.github.com>
2023-02-26 04:30:02 -0600

    CodePush

---
+++ src/test/java/com/example/roaming/RoamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
